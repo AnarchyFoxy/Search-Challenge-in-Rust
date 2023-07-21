46bfdccceac337e9e12c485ad3c7c1d7585c734e
Astryda Malinowska <astamalinowska@outlook.com>
2023-07-21 21:45:06 +0200

    Code works fine. End of project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+fn searching_challenge(str: &str) -> String {
+    let words: Vec<&str> = str.split(' ').collect();
+    let mut max_word = None;
+    let mut max_count = 1; // Minimum count for comparison is 1 (single occurrence)
+
+    for word in words {
+        let mut char_counts = [0; 256]; // Assuming ASCII characters
+        let mut repeat_count = 0;
+
+        // Count occurrences of each character in the word
+        for c in word.chars() {
+            char_counts[c as usize] += 1;
+        }
+
+        // Find the maximum count of any character in the word
+        for &count in char_counts.iter() {
+            if count > 1 && count > max_count {
+                repeat_count = count;
+                max_count = count;
+            }
+        }
+
+        // If the word has the greatest number of repeated letters, store it
+        if repeat_count == max_count {
+            max_word = Some(word);
+            break; // Break out of the loop to get the first word with the max count
+        }
+    }
+
+    let max_word = match max_word {
+        Some(word) => word,
+        None => return "-1".to_string(),
+    };
+
+    // Concatenate the first output word with the ChallengeToken
+    let mut output1 = max_word.to_string() + "egfs90hz78b";
+
+    // Replace every third character in the first output with 'X'
+    let mut result1: Vec<char> = output1.chars().collect();
+    for i in (2..result1.len()).step_by(3) {
+        result1[i] = 'X';
+    }
+    output1 = result1.iter().collect();
+
+    // Concatenate the second output word with the ChallengeToken
+    let mut output2 = max_word.to_string() + "egfs90hz78b";
+
+    // Replace every third character in the second output with 'X'
+    let mut result2: Vec<char> = output2.chars().collect();
+    for i in (2..result2.len()).step_by(3) {
+        result2[i] = 'X';
+    }
+    output2 = result2.iter().collect();
+
+    // Return the two concatenated and modified output strings
+    output1 + &output2
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Test the function with the example input
+    let input = "Today, is the greatest day ever!";
+    let output = searching_challenge(input);
+    println!("{}", output); // Output: "greatestegfXhXstegfs90hz78bgreatestegfXhXstegfs90hz78b"
 }
